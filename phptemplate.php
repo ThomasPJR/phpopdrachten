4b527c6f30c37bd7bdf4005209496e1db8da4bad
thomas ruijters <thomas.ruijters@edu-kw1c.nl>
2020-02-04 12:33:57 +0100

    Firstest

---
+++ phptemplate.php
@@ -0,0 +1,12 @@
+<?php
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="keywords" content="php huiswerk, template">
+        <title> Huiswerk php </title>
+        <link type="stylesheet" rel="text/CSS" href="style.css">
+    </head>
+    <body>
+        <nav>
+
+</html>
