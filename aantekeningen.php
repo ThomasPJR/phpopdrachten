aa34bd24951b597d7d25a64af0df1a78444ca45a
thomas ruijters <thomas.ruijters@edu-kw1c.nl>
2020-02-11 11:36:51 +0100

    Update 1, verbetering template.

---
+++ aantekeningen.php
@@ -0,0 +1,11 @@
+<?php
+
+    /*commentaar*/
+
+    //variabel aanmaken
+    $text = "hallo";
+
+    //De punt werkt als een soort +, het bind de variabel aan de string en zet ze te samen neer.
+    echo $text . " wereld";
+
+?>
