ffedc1f80780f0f97162e56927f40ffd326802c7
Thomas Ruijters <thomas.ruijters@edu-kw1c.nl>
2020-04-05 20:05:31 +0200

    5.2 NOTE Laptop ging kapot deze week, 1 van 2000 euro de ander van 700. PHP storm wilde niet committen dus ik heb mijn best gedaan om het zo snel mogelijk in te leveren

---
+++ hoofdstuk5/5.4/opdracht5.4.php
@@ -0,0 +1,26 @@
+<?php
+/*
+* User: Thomas Ruijters
+* Date: 4/1/2020
+* Time: 13:00 PM
+* File: opdracht5.4.php
+*/
+?>
+
+<?php
+include "../../Includes/header.php";
+?>
+
+<?php
+include "../../Includes/aside.php";
+?>
+
+    <div id="wrapper2">
+        <?php
+        include "script53.php";
+        ?>
+    </div>
+
+<?php
+include "../../Includes/footer.php";
+?>
---
+++ hoofdstuk5/5.4/script.php
@@ -0,0 +1,72 @@
+<h3>
+    Stoplicht
+</h3>
+<form action="script.php" method="post">
+    <label>
+        Komt er een ambulance aan?
+    </label>
+    <input type="radio" value="yes" name="ambulance" id="ambYes"><label for="ambYes">Ja</label>
+    <input type="radio" value="no" name="ambulance" id="ambNo"><label for="ambNo">Nee</label>
+    <br>
+    <label>
+        Stoplicht Kleur?
+    </label>
+    <input type="radio" value="rood" name="color"><label for="red">Rood</label>
+    <input type="radio" value="groen" name="color"><label for="green">Groen</label>
+    <input type="radio" value="oranje" name="color"><label for="orange">Oranje</label>
+    <br>
+    <button type="submit">Verzend</button>
+</form>
+
+<h3>
+    Wat is de situatie en wat moet ik doen?
+</h3>
+<p>
+    Stoplicht staat op
+    <?php
+        echo $_POST['color'];
+    ?>
+     en er komt
+    <?php
+        if($_POST['ambulance'] == 'yes')
+        {
+            echo "een";
+        }
+        else
+        {
+            echo "geen";
+        }
+    ?>
+     ambulance aan.
+</p>
+    <?php
+        if($_POST['ambulance'] == 'yes' || $_POST['color'] == "rood")
+        {
+    ?>
+            <p style="color: red">
+                U moet stoppen
+            </p>
+    <?php
+        }
+    ?>
+
+<?php
+    if($_POST['color'] == "oranje" && $_POST['ambulance'] == 'no')
+    {
+?>
+    <p style="color: orange">
+        Probeer te stoppen als u kunt, anders rij nog door.
+    </p>
+<?php
+    }
+?>
+
+<?php
+    if($_POST['color'] == "groen" && $_POST['ambulance'] == 'no') {
+?>
+        <p style="color: green">
+            U mag door rijden;
+        </p>
+<?php
+    }
+?>
