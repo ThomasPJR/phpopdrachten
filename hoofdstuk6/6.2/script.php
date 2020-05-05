58a2b7be43a40ac67703421f061ff145cca616ce
Thomas Ruijters <thomas.ruijters@edu-kw1c.nl>
2020-05-05 19:15:48 +0200

    5.2 NOTE Laptop ging kapot deze week, 1 van 2000 euro de ander van 700. PHP storm wilde niet committen dus ik heb mijn best gedaan om het zo snel mogelijk in te leveren

---
+++ hoofdstuk6/6.2/script.php
@@ -0,0 +1,76 @@
+<h1>
+    steen papier schaar, wa leuk
+</h1>
+<form id="gameFrm" method="get" action="script.php">
+    <div class="float">
+        <input type="radio"
+                  onchange="document.getElementById('gameFrm').submit();" name="keuze"
+                  value="steen">steen
+    </div>
+    <div class="float"><input type="radio"
+                              onchange="document.getElementById('gameFrm').submit();" name="keuze"
+                              value="papier">papier
+    </div>
+    <div class="float"><input type="radio"
+                              onchange="document.getElementById('gameFrm').submit();" name="keuze"
+                              value="schaar">schaar
+    </div>
+</form>
+
+<?php
+
+if
+(
+    !isset($_SESSION['user1']) ||
+    !isset($_SESSION['user2'])
+)
+{
+    $_SESSION['player'] = 0;
+    $_SESSION['NPC'] = 0;
+}
+
+if (isset($_GET['keuze']))
+{
+    echo "Jij koos: " . $_GET['keuze'];
+
+    //Wat kiest de computer
+    //Random wordt er een getal tussen 0 en 2 gekozen
+
+    $opties = array("steen", "papier", "schaar");
+    $computerkeuzegetal = rand(0,2);
+    $computerkeuze = $opties[$computerkeuzegetal];
+    echo "&nbsp;&nbsp;De computer koos:" . $computerkeuze;
+
+
+    $spelerkeuze = $_GET['keuze'];
+
+    if
+    (
+        $spelerkeuze === "steen" && $computerkeuze === "schaar" ||
+        $spelerkeuze === "papier" && $computerkeuze === "steen" ||
+        $spelerkeuze === "schaar" && $computerkeuze === "papier"
+    )
+    {
+        $_SESSION['player'] += 1
+    }
+
+    elseif
+    (
+        $computerkeuze === "schaar" && $spelerkeuze === "schaar" ||
+        $computerkeuze === "papier" && $spelerkeuze === "steen" ||
+        $computerkeuze === "schaar" && $spelerkeuze === "papier"
+    )
+    {
+        $_SESSION['NPC'] += 1;
+    }
+
+    elseif
+    (
+        $spelerkeuze === "steen" && $computerkeuze === "steen" ||
+        $spelerkeuze === "papier" && $computerkeuze === "papier" ||
+        $spelerkeuze === "schaar" && $computerkeuze === "schaar"
+    )
+    {
+        echo "nope";
+    }
+}
